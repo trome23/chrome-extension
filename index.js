51ba1a3b4bd1710a1cf8f457660bf4d80b3965f8
Tyson Romero <romennoodles23@yahoo.com>
2021-08-11 11:28:09 -0600

    now can open link in new tab

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ const ulEl = document.querySelector("#ul-el")
 let myLeads = []
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    renderLeads()
+    myLeads.push(inputEl.value) //<==push value from input to myLeads array
+    inputEl.value = "" //<==clear input box after clicking button
+    renderLeads() //<== calling function to render content to DOM
 })
 
 function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"  
+    let listItems = "" //<== created variable to store ul list items
+    for (let i = 0; i < myLeads.length; i++) {   //<==for loop to iterate through array
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>  
+                    ${myLeads[i]} 
+                </a>
+            </li> ` //<== using template string instead of string concatenation 
     }
 
-    ulEl.innerHTML = listItems
+    ulEl.innerHTML = listItems  //<== rendering list to DOM
+    
 }
